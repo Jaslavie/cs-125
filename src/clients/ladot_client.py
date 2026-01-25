585c8b0d145db6d65e9e6206fb59128a7bbae920
Jaslavie <jasmineqw45@gmail.com>
2026-01-24 16:52:53 -0800

    feat: api to return most recent occupany status of meters

--- src/clients/ladot_client.py
+++ src/clients/ladot_client.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI
 import os
 import uvicorn
 import requests
-from src.models import Location, Meter
 
 load_dotenv()
 
@@ -21,14 +20,16 @@ Get available meters in the user's destination location
 - Output: list of available meters
 """
 @app.post("/ladot-meters")
-def get_meters_in_area(destLocation: Location):
+def get_meters_in_area(
+        lat: float, # destination latitude
+        lon: float, # destination longitude
+        radius_m: int = 1609.34 # 1 mile in meters
+    ) -> list[dict]:
     url = "https://data.lacity.org/resource/s49e-q6j2.json"
-    
-    radius = 1609.34 # 1 mile radius in meters
 
     # Use the API's built in within_circle() function
     location_boundary = (
-        f"within_circle(latlng, {destLocation.lat}, {destLocation.lon}, {radius})"
+        f"within_circle(latlng, {lat}, {lon}, {radius_m})"
     )
 
     params = {
@@ -36,17 +37,48 @@ def get_meters_in_area(destLocation: Location):
         "$limit": 200
     }
 
-    r = requests.get(url, params=params)
+    r = requests.get(url, params=params, headers=headers)
     return r.json()
 
 
 """
-Get raw dataset of occupancy of each meter
-- Input: Meter object queried by space_id
+Return the most recent occupancy status of each parking meter
+- Input: spaceIds of meters selected in region
 - Output: list of occupancy for meters
 """
 @app.post("/ladot-occupancy")
-def get_occupancy(meters: Meter) -> MeterOccupancies:
+def get_occupancy(spaceids: list[str]) -> dict[str, dict]:
+    url = "https://data.lacity.org/resource/e7h6-4a3e.json"
+
+    if not spaceids: 
+        return {}
+
+    # Create a list of spaceids to check
+    spaceid_list = ",".join(f"'{sid}'" for sid in spaceids)
+
+    params = {
+        "$where": f"spaceid in ({spaceid_list})",
+        "$order": "eventtime DESC",
+        "$limit": 1000
+    }
+
+    r = requests.get(url, params=params, headers=headers)
+    r.raise_for_status()
+
+    # Only keep most recent pair
+    latest = {}
+    for record in r.json():
+        sid = record["spaceid"]
+        
+        # Add the first occurrence in the descending list 
+        if sid not in latest:
+            latest[sid] = {
+                "occupancystate": record.get("occupancystate", "UNKNOWN"),
+                "eventtime": record.get("eventtime")
+            }
+    
+    return latest
+
 
 """
 Get all candidate meters open for the current time
@@ -54,4 +86,4 @@ Get all candidate meters open for the current time
 - Output: space_id of top K results
 """
 # @app.post("/search")
-# def search_meters(request: searchRequest)
+# def search_meters(request: searchRequest):
