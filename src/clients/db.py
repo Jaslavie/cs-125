ce1b3f7f77a3a0b5bd3055985747a38f8049b630
Jaslavie <jasmineqw45@gmail.com>
2026-02-14 16:25:18 -0800

    feat: fn to insert/update meters

--- src/clients/db.py
+++ src/clients/db.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 import psycopg2
 
+from src.models.meter import CandidateMeter
+
 load_dotenv()
 
 def connect_to_db(db_url: str):
@@ -15,4 +17,51 @@ def connect_to_db(db_url: str):
     except Exception as e:
         logger.exception("Unexpected error while connecting to DB")
         raise
-        
+
+def upsert_meter(cur, meter: CandidateMeter) -> None:
+    """
+    Insert/Update a candidate meter to the Meters table
+    Update all fields on conflict
+
+    Input: 
+    - cur (cursor): sends queries through connection
+    - meter: target object to insert or update in db
+    """
+    # Adapter sends SQL string to db server (postgres)
+    # Attempts INSERT with preset %s as placeholder values
+    # If there's an existing spaceid, overwrite original field with new value
+    cur.execute(
+        # SQL query
+        """
+        INSERT INTO meters (
+            spaceid, metertype, lat, lon, address,
+            rate_min, rate_max, time_limit_minutes,
+            occupancy, occupancy_time, updated_at
+        ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        ON CONFLICT (spaceid) DO UPDATE SET
+            metertype          = EXCLUDED.metertype,
+            lat                = EXCLUDED.lat,
+            lon                = EXCLUDED.lon,
+            address            = EXCLUDED.address,
+            rate_min           = EXCLUDED.rate_min,
+            rate_max           = EXCLUDED.rate_max,
+            time_limit_minutes = EXCLUDED.time_limit_minutes,
+            occupancy          = EXCLUDED.occupancy,
+            occupancy_time     = EXCLUDED.occupancy_time,
+            updated_at         = EXCLUDED.updated_at
+        """,
+        # values to insert
+        (
+            meter.spaceid,
+            meter.metertype,
+            meter.location.lat,
+            meter.location.lon,
+            meter.address,
+            meter.rate_per_hour[0],       # rate_min in table
+            meter.rate_per_hour[1],       # rate_max in table
+            meter.time_limit_minutes,
+            meter.occupancy.value,        # store enum as string
+            meter.occupancy_time,
+            datetime.now(timezone.utc),
+        ),
+    )
